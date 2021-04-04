73dc5e26d3dfcc71571ea7ac4b83a0b743a50314
DiegoBulhoes <diegobulhoesm@gmail.com>
2021-04-04 11:58:02 -0400

    Add Grafana

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -43,3 +43,14 @@ resource "helm_release" "rabbitmq" {
     file("${path.module}/rabbitmq.yaml")
   ]
 }
+resource "kubernetes_namespace" "grafana" {
+  metadata {
+    name = "grafana"
+  }
+}
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.grafana.metadata.0.name
+}
